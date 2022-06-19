4a745a2b1c15b4d00979d80110c234ec0b1b58c9
anatolii <batkovich327@gmail.com>
2022-06-19 18:45:47 +0300

    Updated bot functions

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jobfinder-bot.wsgi
--- app.py
+++ app.py
@@ -1,46 +1,39 @@
-# -*- coding: utf-8 -*-
-import re
-import os
 import requests
+import re
 from flask import Flask
+from flask.views import MethodView
 from flask import request
-from flask import jsonify
+import os
 
-# from dotenv import load_dotenv
-
-# from flask_sslify import SSLify
-# load_dotenv()
-
-TOKEN = os.environ.get('TOKEN')
-URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
-headers = {'user-agent': 'my-app/0.0.1'}
-API_URL = os.environ.get('API_URL')
 app = Flask(__name__)
-app.config['DEBUG'] = False
+TOKEN = os.environ.get('TOKEN')
+API_URL = os.environ.get('API_URL')
+TELEGRAM_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 
-# https://api.telegram.org/botTOKEN/setWebhook?url=https://b9f92555.ngrok.io
-# https://api.telegram.org/botTOKEN/deleteWebhook
-
-
-def send_message(chat_id, text='Empty'):
+def get_data_from_api(command):
+    url = API_URL + command
     session = requests.Session()
-    url = URL + 'sendMessage'
-    r = session.get(url, headers=headers,
-                    params=dict(chat_id=chat_id,
-                                text=text,
-                                parse_mode='Markdown', ))  # parse_mode = 'HTML'
+    r = session.get(url).json()
+    return r
 
+
+def send_message(chat_id, msg):
+    session = requests.Session()
+    r = session.get(TELEGRAM_URL, params=dict(chat_id=chat_id,
+                                              text=msg,
+                                              parse_mode='Markdown'))
     return r.json()
 
 
-def parse_text(text):
-    command_pattern = r'/\w+'
-    vacancy_pattern = r'@\w+'  # @\w+\+\w+
-
-    if '/' in text:
-        addresses = {'cities': '/cities', 'pl': '/program_languages'}
-        if '/start' in text or '/help' in text:
+def parse_text(text_msg):
+    '''/start /help, /city /sp, @kiyv @python'''
+    addresses = {'city': '/cities', 'pl': '/program_language'}
+    command_p = r'/\w+'
+    dog_p = r'@\w+'
+    message = 'Невірний запит'
+    if '/' in text_msg:
+        if '/start' in text_msg or '/help' in text_msg:
             message = '''
             Щоб дізнатися які міста доступні введіть `/city`. 
             Щоб дізнатися які мови програмування доступні введіть `/pl` 
@@ -48,88 +41,93 @@ def parse_text(text):
             Наприклад: `@kyiv @python` 
             '''
             return message
-        command = re.search(command_pattern, text).group().replace('/', '')
-        return addresses.get(command, None)
-    elif '@' in text:
-        # @kyiv @python
-        v_search = re.findall(vacancy_pattern, text)
-        commands = [x.replace('@', '') for x in v_search]
-        return commands
+        else:
+            command = re.search(command_p, text_msg).group().replace('/', '')
+            command = addresses.get(command, None)
+            return [command] if command else None
+    elif '@' in text_msg:
+        result = re.findall(dog_p, text_msg)
+        commands = [s.replace('@', '') for s in result]
+        return commands if len(commands) == 2 else None
     else:
-        return None
+        return message
 
 
-def get_api_response(addr):
-    session = requests.Session()
-    url = API_URL + addr
-    r = session.get(url, headers=headers).json()
-    # print(r)
-    return r
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=["POST", "GET"])
 def index():
-    if request.method == 'POST':
-        r = request.get_json()
-        chat_id = r['message']['chat']['id']
-        text_msg = r['message']['text']
+    if request.method == "POST":
+        resp = request.get_json()
+        print(resp)
+        return '<h1> Hi Telegram!!! </h1>'
+    return '<h1> Hi BOT!!! </h1>'
+
+
+class BotAPI(MethodView):
+
+    def get(self):
+        return '<h1> Hi BOT_Class!!! </h1>'
+
+    def post(self):
+        resp = request.get_json()
+        text_msg = resp['message']['text']
+        chat_id = resp['message']['chat']['id']
         tmp = parse_text(text_msg)
-
-        text_msg = 'Запит невірний'
+        text = 'Невірний запит'
+        error_msg = 'По вашому запиту нічого не знайдено'
         if tmp:
-            # print('TMP!')
-            if len(tmp) == 2:
-                address = '/vacancies/?city={}&pl={}'.format(*tmp)
-                print(address)
-                resp = get_api_response(address)
-                if resp:
-                    pices = []
-                    size_of_resp = len(resp)
-                    extra = len(resp) % 10
-                    if size_of_resp < 11:
-                        pices.append(resp)
-                    else:
-                        for i in range(size_of_resp // 10):
-                            y = i * 10
-                            pices.append(resp[y:y + 10])
-                        if extra:
-                            pices.append(resp[y + 10:])
-
-                    text_msg = '''Більше даних на сайті\n https://vacancies-finder.herokuapp.com \nРезультати пошуку:\n'''
-
-                    send_message(chat_id, text_msg)
-                    for part in pices:
-                        message = ''
-                        for v in part:
-                            message += v['title'] + '\n'
-                            # message +=  v['description'] + '\n'
-                            # message +=  'Company:' + v['company'] + '\n'
-                            # message +=  'Date:' + v['timestamp'] + '\n'
-                            message += v['url'] + '\n'
-                            message += '-' * 5 + '\n\n'
-                        send_message(chat_id, message)
-                else:
-                    text_msg = 'За вашим запитом нічого не знайдено.'
-                    send_message(chat_id, text_msg)
-            elif len(tmp) >= 20:
+            if len(tmp) > 10:
                 send_message(chat_id, tmp)
-            else:
-                resp = get_api_response(tmp)
+            elif len(tmp) == 1:
+                resp = get_data_from_api(tmp[0])
                 if resp:
                     message = ''
                     for d in resp:
                         message += '#' + d['slug'] + '\n'
-                    if '/cities' == tmp:
-                        text_msg = 'Доступні міста: \n'
+                    if tmp[0] == '/sp':
+                        msg = "Доступні мови програмування: \n"
                     else:
-                        text_msg = 'Доступні мови програмування: \n'
-                    text_msg += message
-                send_message(chat_id, text_msg)
-        else:
-            send_message(chat_id, text_msg)
-        return jsonify(r)
-    return '<h1> HI Bot <h1>'
+                        msg = "Доступні міста: \n"
+                    send_message(chat_id, msg + message)
+                else:
+                    send_message(chat_id, error_msg)
+            elif len(tmp) == 2:
+                command = '/vacancy/?city={}&pl={}'.format(*tmp)
+                resp = get_data_from_api(command)
+                if resp:
+                    pices = []
+                    size = len(resp)
+                    extra = len(resp) % 10
+                    if size < 11:
+                        pices.append(resp)
+                    else:
+                        for i in range(size // 10):
+                            y = i * 10
+                            pices.append(resp[y:y + 10])
+                        if extra:
+                            pices.append(resp[y + 10:])
+                    # Відправляю в відповідь заголовок
+                    text_msg = 'Результат:\n'
+                    text_msg += '- ' * 10 + '\n'
+                    send_message(chat_id, text_msg)
 
+                    for part in pices:
+                        # для кожної частини формую нову відповідь
+                        # і додаю в той же чат
+                        message = ''
+                        for v in part:
+                            message += v['title'].replace('\t', '').replace('\n', '') + '\n'
+                            url = v['url'].split('?')
+                            message += url[0] + '\n'
+                            message += '-' * 5 + '\n\n'
+                        send_message(chat_id, message)
+                else:
+                    send_message(chat_id, error_msg)
+        else:
+            send_message(chat_id, text)
+        return '<h1> Hi Telegram_Class!!! </h1>'
+
+
+app.add_url_rule(f'/{TOKEN}/', view_func=BotAPI.as_view('bot'))
 
 if __name__ == '__main__':
     app.run()
